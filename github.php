2503e299a3d3bf3d2645f5dbf382a81280842972
sumeta <sumeta.pon@gmail.com>
2020-04-06 13:21:48 +0700

    add github

---
+++ github.php
@@ -0,0 +1,14 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://github.com/sumeta');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+print_r($result);
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
